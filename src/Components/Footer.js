f06770e9294353301e6ccfe8e8b0382dd17e59df
Ryan Kirsch <agalloch88@gmail.com>
2021-05-23 13:22:18 -0400

    info import and ready to launch live

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="Ryan Kirsch" href="https://www.linkedin.com/in/ryan-s-kirsch/">
+                RYAN KIRSCH
               </a>
             </li>
           </ul>
